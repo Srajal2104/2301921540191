13492b7aad82a81a33d55d2f972e314b58145fbe
Srajal <sahusrajal999@gmail.com>
2026-06-26 11:21:12 +0530

    Connected evaluation APIs

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,6 +1,19 @@
-export const logger = (message, data = null) => {
-  console.log(
-    `[LOG] ${new Date().toISOString()} : ${message}`,
-    data
-  );
+import {sendLog} from "../services/api";
+
+
+export const logger=(message,data=null)=>{
+
+
+console.log(
+ `[LOG] ${message}`,
+ data
+);
+
+
+sendLog(
+ "info",
+ message
+);
+
+
 };
--- src/services/api.js
+++ src/services/api.js
@@ -1,26 +1,138 @@
 import { logger } from "../middleware/logger";
 
-const BASE_URL = "http://localhost:8080";
 
-export const sendNotification = async (notification) => {
-  logger("Sending notification", notification);
+const BASE_URL =
+"http://4.224.186.213/evaluation-service";
 
-  const response = await fetch(`${BASE_URL}/notifications`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(notification),
-  });
 
-  return await response.json();
+let token = "";
+
+
+export const registerUser = async(user)=>{
+
+    logger("Register API called",user);
+
+
+    const res = await fetch(
+        `${BASE_URL}/register`,
+        {
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify(user)
+        }
+    );
+
+
+    return await res.json();
+
 };
 
 
-export const getNotifications = async () => {
-  logger("Fetching notifications");
 
-  const response = await fetch(`${BASE_URL}/notifications`);
+export const authenticate = async(data)=>{
 
-  return await response.json();
+
+    logger("Auth API called");
+
+
+    const res = await fetch(
+        `${BASE_URL}/auth`,
+        {
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify(data)
+        }
+    );
+
+
+    const result = await res.json();
+
+
+    token=result.access_token;
+
+
+    localStorage.setItem(
+        "token",
+        token
+    );
+
+
+    return result;
+
+};
+
+
+
+
+
+export const sendLog = async(
+    level,
+    message
+)=>{
+
+
+await fetch(
+ `${BASE_URL}/logs`,
+ {
+  method:"POST",
+  headers:{
+   "Content-Type":"application/json"
+  },
+
+  body:JSON.stringify({
+
+    stack:"frontend",
+    level:level,
+    package:"api",
+    message:message
+
+  })
+
+ }
+);
+
+
+};
+
+
+
+
+
+export const apiRequest = async(
+url,
+options={}
+)=>{
+
+
+const token =
+localStorage.getItem("token");
+
+
+const res =
+await fetch(
+ url,
+ {
+
+ ...options,
+
+ headers:{
+
+  "Content-Type":"application/json",
+
+  Authorization:
+  `Bearer ${token}`
+
+ }
+
+ }
+);
+
+
+return await res.json();
+
+
 };
